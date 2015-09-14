0ef8ff79f7c08890e6ddd2b8b8ae4ad7a6e0663b
jhumigas <jhumigas@gmail.com>
2015-09-14 18:19:23 +0200

    Test

---
+++ app/assets/javascripts/ng-app/app.js
@@ -0,0 +1,58 @@
+angular
+    .module('myApp', [
+        'ngAnimate',
+        'ui.router',
+    //    'ui.bootstrap',
+        'templates'
+    ])
+    .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+            $stateProvider
+                .state('home', {
+                    url: '/',
+                    templateUrl: '../assets/home.html',
+                    controller: 'HomeCtrl'
+                })
+                .state('about', {
+                    url: '/about',
+                    templateUrl: '../assets/about-us.html',
+                })
+                .state('services', {
+                    url: '/services',
+                    templateUrl: '../assets/services.html',
+                })
+                .state('portofolio', {
+                    url: '/portofolio',
+                    templateUrl: '../assets/portofolio.html',
+                })
+                .state('blog', {
+                    url: '/blog',
+                    templateUrl: '../assets/blog.html',
+                })
+                .state('contact', {
+                    url: '/contact',
+                    templateUrl: '../assets/contact-us.html',
+                })
+                .state('faq', {
+                    url: '/faq',
+                    templateUrl: '../assets/faq.html',
+                })
+                .state('terms', {
+                    url: '/terms',
+                    templateUrl: '../assets/terms.html',
+                })
+                .state('career', {
+                    url: '/career',
+                    templateUrl: '../assets/career.html',
+                })
+                .state('blogItem', {
+                    url: '/blogItem',
+                    templateUrl: '../assets/blog-item.html',
+                });
+                
+                
+            // default fall back route
+            $urlRouterProvider.otherwise('/');
+        
+            // enable HTML5 Mode for SEO
+            // $locationProvider.html5Mode(true);
+    });
---
+++ app/assets/javascripts/ng-app/controllers/home.js
@@ -0,0 +1,5 @@
+angular.module('myApp')
+    .controller('HomeCtrl', function ($scope) {
+        $scope.things = ['Angular', 'Rails 4.1', 'UI Router', 'Together!!'];
+        
+    });
